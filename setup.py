0f02c13b78280664fa95afdb2fdfe4b6f93d641d
Alice Bevan-McGregor <alice@gothcandy.com>
2014-01-14 01:22:39 -0500

    Meta.

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import sys, os
+from setuptools import setup, find_packages
+
+setup(
+        name = "Brave Core Service API Bindings",
+        version = "0.1",
+        description = "Python API bindings for both utilizing and hosting the secure RPC mechanism.",
+        author = "Alice Bevan-McGregor",
+        author_email = "alice@gothcandy.com",
+        license = "MIT",
+        
+        packages = find_packages(),
+        include_package_data = True,
+        zip_safe = False,
+        namespace_packages = ['brave'],
+        
+        tests_require = ['nose', 'webtest', 'coverage'],
+        test_suite = 'nose.collector',
+        
+        install_requires = [
+                'WebOb',
+                'marrow.util',
+                'ecdsa',
+                'futures'
+            ],
+        
+    )
