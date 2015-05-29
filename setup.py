df344d2bb9499cf2978010af630de44f992dfce1
Josiah Boning <jboning@gmail.com>
2015-05-29 16:06:03 -0700

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = "braveapi",
-        version = "0.2",
+        version = "0.3",
         description = "Python API bindings for both utilizing and hosting the secure RPC mechanism.",
         author = "Alice Bevan-McGregor",
         author_email = "alice@gothcandy.com",
